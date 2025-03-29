6781de2f81e73f6292080f8aaa608b6a1e1cdbd9
Bodi Ali <bodiali59@gmail.com>
2025-03-29 09:18:48 +0200

    Add coverage config

--- vite.config.js
+++ vite.config.js
@@ -8,5 +8,8 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
     setupFiles: "./src/tests/setup/setup.js",
+    coverage: {
+      provider: "istanbul",
+    },
   },
 });
