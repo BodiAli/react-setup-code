cd422a17f58c13a05c88f0b80ea6de015ad49300
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2025-06-29 06:53:00 +0300

    Change setup file to typescript file

--- src/tests/setup/setup.ts
+++ src/tests/setup/setup.ts
@@ -1,12 +1,10 @@
-import { expect, afterEach } from "vitest";
+import "@testing-library/jest-dom"; // brings in the matcher definitions
+import { afterEach, vi } from "vitest";
 import { cleanup } from "@testing-library/react";
-import * as matchers from "@testing-library/jest-dom/matchers";
-
-expect.extend(matchers);
 
 Object.defineProperty(window, "matchMedia", {
   writable: true,
-  value: vi.fn().mockImplementation((query) => ({
+  value: vi.fn().mockImplementation((query: string) => ({
     matches: false,
     media: query,
     onchange: null,
--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: "./src/tests/setup/setup.js",
+    setupFiles: "./src/tests/setup/setup.ts",
     coverage: {
       provider: "istanbul",
     },
